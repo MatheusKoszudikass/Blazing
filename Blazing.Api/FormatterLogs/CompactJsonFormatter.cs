e1f960e0819778c2a6ef4fcb8dafa876bb3c66f4
Matheus Gomes <matheusprgc@gmail.com>
2024-08-19 16:23:17 -0300

    Adicionar pasta FormatterLogs com as classes CompactJsonFormatter, EventIdHash e RenderedCompactJsonFormatter para formatação dos logs em JSON

---
+++ Blazing.Api/FormatterLogs/CompactJsonFormatter.cs
@@ -0,0 +1,102 @@
+﻿
+using System.Globalization;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Json;
+using Serilog.Parsing;
+
+namespace Blazing.Api.FormatterLogs
+{
+    public class CompactJsonFormatter : ITextFormatter
+    {
+        readonly JsonValueFormatter _valueFormatter;
+
+        public CompactJsonFormatter(JsonValueFormatter? valueFormatter = null)
+        {
+            _valueFormatter = valueFormatter ?? new JsonValueFormatter(typeTagName: "$type");
+        }
+
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            FormatEvent(logEvent, output, _valueFormatter);
+            output.WriteLine();
+        }
+
+        public static void FormatEvent(LogEvent logEvent, TextWriter output, JsonValueFormatter valueFormatter)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+            if (valueFormatter == null) throw new ArgumentNullException(nameof(valueFormatter));
+
+            // Ajusta o timestamp para UTC +3
+            var adjustedTimestamp = logEvent.Timestamp.ToUniversalTime().AddHours(3);
+
+            output.Write("{\"@t\":\"");
+            output.Write(adjustedTimestamp.ToString("O"));
+            output.Write("\",\"@mt\":");
+            JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
+
+            var tokensWithFormat = logEvent.MessageTemplate.Tokens
+                .OfType<PropertyToken>()
+                .Where(pt => pt.Format != null);
+
+            if (tokensWithFormat.Any())
+            {
+                output.Write(",\"@r\":[");
+                var delim = "";
+                foreach (var r in tokensWithFormat)
+                {
+                    output.Write(delim);
+                    delim = ",";
+                    var space = new StringWriter();
+                    r.Render(logEvent.Properties, space, CultureInfo.InvariantCulture);
+                    JsonValueFormatter.WriteQuotedJsonString(space.ToString(), output);
+                }
+                output.Write(']');
+            }
+
+            if (logEvent.Level != LogEventLevel.Information)
+            {
+                output.Write(",\"@l\":\"");
+                output.Write(logEvent.Level);
+                output.Write('\"');
+            }
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"@x\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
+            }
+
+            if (logEvent.TraceId != null)
+            {
+                output.Write(",\"@tr\":\"");
+                output.Write(logEvent.TraceId.Value.ToHexString());
+                output.Write('\"');
+            }
+
+            if (logEvent.SpanId != null)
+            {
+                output.Write(",\"@sp\":\"");
+                output.Write(logEvent.SpanId.Value.ToHexString());
+                output.Write('\"');
+            }
+
+            foreach (var property in logEvent.Properties)
+            {
+                var name = property.Key;
+                if (name.Length > 0 && name[0] == '@')
+                {
+                    name = '@' + name;
+                }
+
+                output.Write(',');
+                JsonValueFormatter.WriteQuotedJsonString(name, output);
+                output.Write(':');
+                valueFormatter.Format(property.Value, output);
+            }
+
+            output.Write('}');
+        }
+    }
+}
