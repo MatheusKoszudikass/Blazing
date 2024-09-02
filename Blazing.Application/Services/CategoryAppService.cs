eb8c4bd8f31a9fc2b067e931cb4a82ec08cc4831
Matheus Gomes <matheusprgc@gmail.com>
2024-09-02 18:49:42 -0300

    style: palavas erradas e espaçamentos.

--- Blazing.Application/Services/CategoryAppService.cs
+++ Blazing.Application/Services/CategoryAppService.cs
@@ -25,12 +25,12 @@ namespace Blazing.Application.Services
         /// Adds a list of category to the domain.
         /// </summary>
         /// <param name="categoryDto">The list of categoryDtos to be added.</param>
-        /// <returns>The list of categoryDtos that have been added.</returns>
+        /// <returns>The list of categoryDto that have been added.</returns>
         public async Task<IEnumerable<CategoryDto?>> AddCategory(IEnumerable<CategoryDto> categoryDto, CancellationToken cancellationToken)
         {
             var category = _mapper.Map<IEnumerable<Category>>(categoryDto);
 
-            var categoryResult = await _categoriaDomainService.Add(category);
+            var categoryResult = await _categoriaDomainService.Add(category, cancellationToken);
 
             categoryDto = _mapper.Map<IEnumerable<CategoryDto>>(categoryResult);
 
@@ -38,74 +38,74 @@ namespace Blazing.Application.Services
         }
 
         /// <summary>
-        /// Updates an existing categoryDtos based on the provided ID.
+        /// Updates an existing categoryDto based on the provided ID.
         /// </summary>
-        /// <param name="id">The ID of the categoryDtos to update.</param>
-        /// <param name="categoryDtos">The categoryDtos object containing the updated data.</param>
-        /// <returns>The updated categoryDtos, if found.</returns>
-        public async Task<IEnumerable<CategoryDto?>> UpdateCategory(IEnumerable<Guid> id, IEnumerable<CategoryDto> categoryDtos, IEnumerable<CategoryDto> categoriesDtosUpdate, CancellationToken cancellationToken)
+        /// <param name="id">The ID of the categoryDto to update.</param>
+        /// <param name="categoryDto">The categoryDto object containing the updated data.</param>
+        /// <returns>The updated categoryDto, if found.</returns>
+        public async Task<IEnumerable<CategoryDto?>> UpdateCategory(IEnumerable<Guid> id, IEnumerable<CategoryDto> categoryDto, IEnumerable<CategoryDto> categoriesDtosUpdate, CancellationToken cancellationToken)
         {
-            var category =  _mapper.Map<IEnumerable<Category>>(categoryDtos);
+            var category =  _mapper.Map<IEnumerable<Category>>(categoryDto);
             var categoryUpdate = _mapper.Map<IEnumerable<Category>>(categoriesDtosUpdate);
 
-            var categoryResultDto =  await _categoriaDomainService.Update(id, category, categoryUpdate);
+            var categoryResultDto =  await _categoriaDomainService.Update(id, category, categoryUpdate, cancellationToken);
 
-            categoryDtos = _mapper.Map<IEnumerable<CategoryDto>>(categoryResultDto);
+            categoryDto = _mapper.Map<IEnumerable<CategoryDto>>(categoryResultDto);
 
-            return categoryDtos;
+            return categoryDto;
         }
 
         /// <summary>
-        /// Deletes categoryDtos based on a list of provided IDs.
+        /// Deletes categoryDto based on a list of provided IDs.
         /// </summary>
-        /// <param name="id">The list of categoryDtos IDs to be deleted.</param>
-        /// <returns>The list of categoryDtos that were deleted.</returns>
-        public async Task<IEnumerable<CategoryDto?>> DeleteCategory(IEnumerable<Guid> id, IEnumerable<CategoryDto?> categoryDtos, CancellationToken cancellationToken)
+        /// <param name="id">The list of categoryDto IDs to be deleted.</param>
+        /// <returns>The list of categoryDto that were deleted.</returns>
+        public async Task<IEnumerable<CategoryDto?>> DeleteCategory(IEnumerable<Guid> id, IEnumerable<CategoryDto> categoryDto, CancellationToken cancellationToken)
         {
-            var category = _mapper.Map<IEnumerable<Category>>(categoryDtos);
+            var category = _mapper.Map<IEnumerable<Category>>(categoryDto);
 
-            await _categoriaDomainService.Delete(id, category);
+            await _categoriaDomainService.Delete(id, category, cancellationToken);
 
-            return categoryDtos;
+            return categoryDto;
         }
 
         /// <summary>
-        /// Gets a specific categoryDtos based on the given ID.
+        /// Gets a specific categoryDto based on the given ID.
         /// </summary>
-        /// <param name="id">The ID of the categoryDtos to get.</param>
-        /// <returns>The categoryDtos corresponding to the given ID.</returns>
-        public async Task<IEnumerable<CategoryDto?>> GetById(IEnumerable<Guid> id, IEnumerable<CategoryDto?> categoryDtos, CancellationToken cancellationToken)
+        /// <param name="id">The ID of the categoryDto to get.</param>
+        /// <returns>The categoryDto corresponding to the given ID.</returns>
+        public async Task<IEnumerable<CategoryDto?>> GetById(IEnumerable<Guid> id, IEnumerable<CategoryDto> categoryDto, CancellationToken cancellationToken)
         {
-            var category = _mapper.Map<IEnumerable<Category>>(categoryDtos);
+            var category = _mapper.Map<IEnumerable<Category>>(categoryDto);
 
             await _categoriaDomainService.GetById(id, category, cancellationToken);
 
-            return categoryDtos;
+            return categoryDto;
         }
 
 
         /// <summary>
-        /// Gets all categoryDtos from the domain.
+        /// Gets all categoryDto from the domain.
         /// </summary>
-        /// <param name="categoryDtos">The list of categoryDtos to be added.</param>
-        /// <returns>The list of all categoryDtos.</returns>
-        public async Task<IEnumerable<CategoryDto?>> GetAll(IEnumerable<CategoryDto?> categoryDtos, CancellationToken cancellationToken)
+        /// <param name="categoryDto">The list of categoryDto to be added.</param>
+        /// <returns>The list of all categoryDto.</returns>
+        public async Task<IEnumerable<CategoryDto?>> GetAll(IEnumerable<CategoryDto?> categoryDto, CancellationToken cancellationToken)
         {
-            var category = _mapper.Map<IEnumerable<Category>>(categoryDtos);
+            var category = _mapper.Map<IEnumerable<Category>>(categoryDto);
 
             await _categoriaDomainService.GetAll(category, cancellationToken);
 
-            return categoryDtos;
+            return categoryDto;
         }
 
         /// <summary>
         /// Checks if categories exist based on the provided flag.
         /// </summary>
-        /// <param name="existsCategories">A boolean flag indicating the existence check.</param>
-        public async Task<bool?> ExistsCategories(bool id, bool existsName, IEnumerable<CategoryDto?> categoryDtos)
+        /// <param name="categoryDto">A boolean flag indicating the existence check.</param>
+        public async Task<bool?> ExistsCategories(bool id, bool existsName, IEnumerable<CategoryDto?> categoryDto, CancellationToken cancellationToken)
         {
-            var category = _mapper.Map<IEnumerable<Category>>(categoryDtos);
-            await _categoriaDomainService.ExistsAsync(id, existsName, category);
+            var category = _mapper.Map<IEnumerable<Category>>(categoryDto);
+            await _categoriaDomainService.ExistsAsync(id, existsName, category, cancellationToken);
 
             await Task.CompletedTask;
             return id;
