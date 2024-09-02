cff8f16d97acb1ee1b41b59ed9e305ee6327673b
Matheus Gomes <matheusprgc@gmail.com>
2024-09-02 18:10:03 -0300

    chore: Data annotation.

--- Blazing.Application/Dto/AddressDto.cs
+++ Blazing.Application/Dto/AddressDto.cs
@@ -1,5 +1,6 @@
 ﻿using Blazing.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blazing.Application.Dto
 {
@@ -8,37 +9,22 @@ namespace Blazing.Application.Dto
     /// DTO responsible for the address.
     /// </summary>
     public sealed class AddressDto : BaseEntityDto
-    { 
-        [Required(ErrorMessage = "Rua é obrigatória.")]
-        [StringLength(100, ErrorMessage = "Rua deve ter entre 3 e 100 caracteres.", MinimumLength = 3)]
+    {
+        public Guid UserId { get; set; }
+
         public string? Street { get; set; }
 
-        [Required(ErrorMessage = "Número é obrigatorio.")]
-        [StringLength(10, ErrorMessage = "Número deve ter entre 1 e 10 caracteres.", MinimumLength = 1)]
         public string? Number { get; set; }
 
-        [Required(ErrorMessage = "Complemento é obrigatorio.")]
-        [StringLength(100, ErrorMessage = "Complemento deve ter entre 3 e 100 caracteres.", MinimumLength = 3)]
         public string? Complement { get; set; }
 
-        [Required(ErrorMessage = "Bairro é obrigatorio.")]
-        [StringLength(50, ErrorMessage = "Bairro deve ter entre 3 e 50 caracteres.", MinimumLength = 3)]
         public string? Neighborhood { get; set; }
 
-        [Required(ErrorMessage = "Cidade é obrigatorio.")]
-        [StringLength(50, ErrorMessage = "Cidade deve ter entre 3 e 50 caracteres.", MinimumLength = 3)]
         public string? City { get; set; }
 
-        [Required(ErrorMessage = "Estado é obrigatorio.")]
-        [StringLength(2, ErrorMessage = "Estado deve ter 2 caracteres.", MinimumLength = 2)]
         public string? State { get; set; }
 
-        [Required(ErrorMessage = "CEP é obrigatorio.")]
-        [StringLength(8, ErrorMessage = "CEP deve ter 8 caracteres.", MinimumLength = 8)]
         public string? PostalCode { get; set; }
-
-        [Required(ErrorMessage = "Usuario é obrigatorio.")]
-        public Guid UserId { get; set; }
     }
 #endregion
 }
