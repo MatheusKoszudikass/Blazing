981afeb3075ac01265b78b9788738069e10f7e2b
Matheus Gomes <matheusprgc@gmail.com>
2024-09-05 17:31:10 -0300

    refactor: Contexto e o mapper apenas relacionado ao Ecommerce.

---
+++ Blazing.infrastructure/Blazing.Ecommerce/Dependencies/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Blazing.Ecommerce.Data;
+
+
+namespace Blazing.Ecommerce.Dependency
+{
+    public class DependencyInjection(BlazingDbContext AppDbContext, IMapper mapper)
+    {
+        public readonly BlazingDbContext _appContext = AppDbContext;
+
+        public readonly IMapper _mapper = mapper;
+    }
+}
