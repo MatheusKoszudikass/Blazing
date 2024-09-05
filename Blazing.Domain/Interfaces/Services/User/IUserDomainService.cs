6c020d83aacf9be6481cba48169f948dc7e893da
Matheus Gomes <matheusprgc@gmail.com>
2024-09-05 17:02:59 -0300

    feat: Contrato de interface para personalizar o serviço do usuário na camada domain tendo dependencia da interface ICrudDomainService.

---
+++ Blazing.Domain/Interfaces/Services/User/IUserDomainService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazing.Domain.Interfaces.Services.User
+{
+    public interface IUserDomainService : ICrudDomainService<Entities.User>
+    {
+        Task<bool> UserExistsAsync(bool id, bool userName, bool userEmail, IEnumerable<Entities.User> users, CancellationToken cancellationToken);
+    }
+}
