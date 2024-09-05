71d93f5f3eda628c1c93380513354ee2151254cc
Matheus Gomes <matheusprgc@gmail.com>
2024-09-05 17:32:34 -0300

    refactor: Separação de responsabilidade só para camada do Ecommerce.

---
+++ Blazing.infrastructure/Blazing.Ecommerce/Repository/ICategoryInfrastructureRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blazing.Application.Dto;
+
+
+namespace Blazing.Ecommerce.Repository
+{
+    public interface ICategoryInfrastructureRepository
+    {
+        Task<IEnumerable<CategoryDto?>> AddCategories(IEnumerable<CategoryDto> productDto, CancellationToken cancellationToken);
+
+        Task<IEnumerable<CategoryDto?>> UpdateCategory(IEnumerable<Guid> id, IEnumerable<CategoryDto> productDto, CancellationToken cancellationToken);
+
+        Task<IEnumerable<CategoryDto?>> DeleteCategory(IEnumerable<Guid> id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<CategoryDto?>> GetCategoryById(IEnumerable<Guid> id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<CategoryDto?>> GetAll(CancellationToken cancellationToken);
+    }
+}
