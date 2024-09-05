b660471435c709ac092a55b0c6bf53c0b209bbbd
Matheus Gomes <matheusprgc@gmail.com>
2024-09-05 17:28:58 -0300

    feat: Separação de responsabilidade da injeção de depencias da camada do Blazing.Api.

---
+++ Blazing.infrastructure/Blazing.Ecommerce/Dependencies/ConfigServiceCollectionExtensiosInfraEcommerce.cs
@@ -0,0 +1,44 @@
+﻿using Blazing.Application.Dto;
+using Blazing.Application.Interfaces.Category;
+using Blazing.Application.Interfaces.Product;
+using Blazing.Application.Services;
+using Blazing.Domain.Entities;
+using Blazing.Domain.Interfaces.Services;
+using Blazing.Domain.Interfaces.Services.User;
+using Blazing.Domain.Services;
+using Blazing.Ecommerce.Data;
+using Blazing.Ecommerce.Repository;
+using Blazing.Ecommerce.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazing.Ecommerce.Dependency
+{
+    public static class ConfigServiceCollectionExtensiosInfraEcommerce
+    {
+
+        public static IServiceCollection AddConfigInfraEcommerce(
+            this IServiceCollection Services, IConfiguration Config)
+        {
+            Services.AddDbContext<BlazingDbContext>(options =>
+                  options.UseSqlServer(Config.GetConnectionString("Blazing"), b => b.MigrationsAssembly("Blazing.Ecommerce")));
+
+
+
+            //Product dependencies
+            Services.AddScoped<IProductInfrastructureRepository, ProductInfrastructureRepository>();
+            Services.AddScoped<IProductAppService<ProductDto>, ProductAppService>();
+            Services.AddScoped<ICrudDomainService<Product>, ProductDomainService>();
+
+            //Category dependencies
+            Services.AddScoped<ICategoryInfrastructureRepository, CategoryInfrastructureRepository>();
+            Services.AddScoped<ICategoryAppService<CategoryDto>, CategoryAppService>();
+            Services.AddScoped<ICrudDomainService<Category>, CategoryDomainService>();
+
+            //Users
+            Services.AddScoped<IUserDomainService, UserDomainService>();
+            return Services;
+        }
+    }
+}
