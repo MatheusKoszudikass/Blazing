a4f0bffb3a0e9dc561e4873ae37eabecdff63db8
Matheus Gomes <matheusprgc@gmail.com>
2024-09-10 07:44:24 -0300

    chore: Renomeação de pastas.

---
+++ Blazing.infrastructure/Blazing.Identity/Interface/IUserInfrastructureRepository.cs
@@ -0,0 +1,33 @@
+﻿using Blazing.Application.Dto;
+using Blazing.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blazing.Identity.Interface
+{
+    #region ContratcsUserIdentity
+
+    /// <summary>
+    /// Represents a repository for managing user infrastructure.
+    /// </summary>
+    public interface IUserInfrastructureRepository
+    {
+        Task<UserCreationResult?> AddUsersAsync(IEnumerable<UserDto> UserDto, CancellationToken cancellationToken);
+
+        Task<UserCreationResult?> UpdateUsersAsync(IEnumerable<UserDto> userDto, CancellationToken cancellationToken);
+
+        Task<UserCreationResult?> DeleteUsersAsync(IEnumerable<string> id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<UserDto?>> GetUsersByIdAsync(IEnumerable<Guid> id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<UserDto?>> GetAllUsersAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+        Task<SignInResult> LoginAsync(Login login,
+            CancellationToken cancellationToken);
+
+        Task<string> GeneratePasswordResetTokenAsync(string email, CancellationToken cancellationToken);
+
+        Task<IdentityResult> ResetPasswordAsync(string email, string token, string newPassword, CancellationToken cancellationToken);
+    }
+
+    #endregion
+}
