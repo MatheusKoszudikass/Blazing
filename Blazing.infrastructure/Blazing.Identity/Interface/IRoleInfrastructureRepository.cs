a30e2a05d92863aa924399adf354e9fe2d696556
Matheus Gomes <matheusprgc@gmail.com>
2024-09-10 07:39:00 -0300

    feat: Contrato das funções do usuário herdando de uma interface generica.

---
+++ Blazing.infrastructure/Blazing.Identity/Interface/IRoleInfrastructureRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blazing.Domain.Interfaces.Repository;
+using Blazing.Identity.Dto;
+using Blazing.Identity.Entities;
+using Blazing.Identity.Interface;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blazing.Identity.Interface
+{
+    public interface IRoleInfrastructureRepository : ICrudInfrastructureIdentityRepository<IdentityResult, ApplicationRoleDto>
+    {
+
+    }
+}
