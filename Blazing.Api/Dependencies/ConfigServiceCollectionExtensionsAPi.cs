d9291f51f17918ad31bc9f7e6058bc0541917fc9
Matheus Gomes <matheusprgc@gmail.com>
2024-09-15 07:12:22 -0300

    feat: Depencia do mapeamente personalizado do produto.

--- Blazing.Api/Dependencies/ConfigServiceCollectionExtensionsAPi.cs
+++ Blazing.Api/Dependencies/ConfigServiceCollectionExtensionsAPi.cs
@@ -20,6 +20,8 @@ namespace Blazing.Api.Dependencies
         {
             services.AddAutoMapper(typeof(BlazingProfile));
 
+            services.AddScoped<ProductMapping>();
+
             services.AddSwaggerGen();
 
             return services;
