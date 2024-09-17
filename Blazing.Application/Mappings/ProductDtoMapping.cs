e8629b8a08983c6620c88404f670cef65824f69d
Matheus Gomes <matheusprgc@gmail.com>
2024-09-17 06:42:39 -0300

    feat: Classes utilizada para mapeamento manual. Melhora o desempenho da aplicação.

---
+++ Blazing.Application/Mappings/ProductDtoMapping.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazing.Application.Dto;
+using Blazing.Domain.Entities;
+
+namespace Blazing.Application.Mappings
+{
+    public class ProductDtoMapping
+    {
+        public virtual IEnumerable<ProductDto?> ReturnProductDto(IEnumerable<Product>? products, CancellationToken cancellationToken)
+        {
+            var productResultDto = new List<ProductDto?>();
+            foreach (var product in products)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                var productDto = new ProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    Currency = product.Currency,
+                    CategoryId = product.CategoryId,
+                    Brand = product.Brand,
+                    SKU = product.SKU,
+                    StockQuantity = product.StockQuantity,
+                    StockLocation = product.StockLocation,
+                    DimensionsId = product.DimensionsId,
+                    AssessmentId = product.AssessmentId,
+                    AttributesId = product.AttributesId,
+                    AvailabilityId = product.AvailabilityId,
+                    ImageId = product.ImageId,
+
+                    Dimensions = ConvertToDimensionsDto(product.Dimensions),
+                    Assessment = ConvertToAssessmentDto(product.Assessment),
+                    Attributes = ConvertToAttributeDto(product.Attributes),
+                    Availability = ConvertToAvailabilityDto(product.Availability),
+                    Image = ConvertToImageDto(product.Image)
+                };
+
+                if (product.Assessment?.RevisionDetail != null)
+                {
+                    foreach (var revision in product.Assessment.RevisionDetail)
+                    {
+                        var revisionDto = ConvertToRevisionDto(revision);
+                        productDto.Assessment.RevisionDetail.ToList().Add(revisionDto);
+                    }
+                }
+
+                productResultDto.Add(productDto);
+            }
+            return productResultDto;
+        }
+
+        public virtual DimensionsDto? ConvertToDimensionsDto(Dimensions? dimensions)
+        {
+            if (dimensions == null) return null;
+
+            return new DimensionsDto
+            {
+                Id = dimensions.Id,
+                Width = dimensions.Width,
+                Height = dimensions.Height,
+                Depth = dimensions.Depth
+            };
+        }
+
+        public virtual AssessmentDto? ConvertToAssessmentDto(Assessment? assessment)
+        {
+            if (assessment == null) return null;
+
+            return new AssessmentDto
+            {
+                Id = assessment.Id,
+                NumberOfReviews = assessment.NumberOfReviews,
+                RevisionId = assessment.RevisionId,
+            };
+        }
+
+        public virtual AttributeDto? ConvertToAttributeDto(Attributes? attribute)
+        {
+            if (attribute == null) return null;
+            return new AttributeDto
+            {
+                Id = attribute.Id,
+                Color = attribute.Color,
+                Material = attribute.Material,
+                Model = attribute.Model
+            };
+        }
+
+        public virtual AvailabilityDto? ConvertToAvailabilityDto(Availability? availability)
+        {
+            if (availability == null) return null;
+
+            return new AvailabilityDto
+            {
+                Id = availability.Id,
+                IsAvailable = availability.IsAvailable,
+                EstimatedDeliveryDate = availability.EstimatedDeliveryDate
+            };
+        }
+
+        public virtual ImageDto? ConvertToImageDto(Image? image)
+        {
+            if (image == null) return null;
+
+            return new ImageDto
+            {
+                Id = image.Id,
+                Url = image.Url,
+                AltText = image.AltText
+            };
+        }
+
+        public virtual RevisionDto ConvertToRevisionDto(Revision revision)
+        {
+            return new RevisionDto
+            {
+                Id = revision.Id,
+                Date = revision.Date,
+                User = revision.User != null ? new UserDto
+                {
+                    Id = revision.User.Id,
+                    UserName = revision.User.UserName,
+                    Email = revision.User.Email
+                } : null
+            };
+        }
+    }
+}
---
+++ Blazing.Application/Mappings/ProductMapping.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazing.Application.Dto;
+using Blazing.Domain.Entities;
+
+namespace Blazing.Application.Mappings
+{
+    public class ProductMapping
+    {
+        public virtual IEnumerable<Product?> ReturnProduct(IEnumerable<ProductDto>? productsDto)
+        {
+            var productResult = new List<Product>();
+            foreach (var productDto in productsDto)
+            {
+                var products = new Product
+                {
+                    Id = productDto.Id,
+                    Name = productDto.Name,
+                    Description = productDto.Description,
+                    Price = productDto.Price,
+                    Currency = productDto.Currency,
+                    CategoryId = productDto.CategoryId,
+                    Brand = productDto.Brand,
+                    SKU = productDto.SKU,
+                    StockQuantity = productDto.StockQuantity,
+                    StockLocation = productDto.StockLocation,
+                    DimensionsId = productDto.DimensionsId,
+                    AssessmentId = productDto.AssessmentId,
+                    AttributesId = productDto.AttributesId,
+                    AvailabilityId = productDto.AvailabilityId,
+                    ImageId = productDto.ImageId,
+
+                    Dimensions = ConvertToDimensions(productDto.Dimensions),
+                    Assessment = ConvertToAssessment(productDto.Assessment),
+                    Attributes = ConvertToAttributes(productDto.Attributes),
+                    Availability = ConvertToAvailability(productDto.Availability),
+                    Image = ConvertToImage(productDto.Image)
+                };
+
+                if (productDto.Assessment?.RevisionDetail != null)
+                {
+                    foreach (var revision in productDto.Assessment.RevisionDetail)
+                    {
+                        var revisionDto = ConvertToRevision(revision);
+                        if (revisionDto != null) products.Assessment.RevisionDetail.ToList().Add(revisionDto);
+                    }
+                }
+
+                productResult.Add(products);
+            }
+            return productResult;
+        }
+
+        public virtual Dimensions? ConvertToDimensions(DimensionsDto? dimensionsDto)
+        {
+            if (dimensionsDto == null) return null;
+
+            return new Dimensions
+            {
+                Id = dimensionsDto.Id,
+                Width = dimensionsDto.Width,
+                Height = dimensionsDto.Height,
+                Depth = dimensionsDto.Depth
+            };
+        }
+
+        public virtual Assessment? ConvertToAssessment(AssessmentDto? assessmentDto)
+        {
+            if (assessmentDto == null) return null;
+
+            return new Assessment
+            {
+                Id = assessmentDto.Id,
+                NumberOfReviews = assessmentDto.NumberOfReviews,
+                RevisionId = assessmentDto.RevisionId
+            };
+        }
+
+        public virtual Revision? ConvertToRevision(RevisionDto? revisionDto)
+        {
+            if (revisionDto == null) return null;
+
+            return new Revision
+            {
+                Id = revisionDto.Id,
+                Date = revisionDto.Date,
+                User = revisionDto.User != null ? new User
+                {
+                    Id = revisionDto.User.Id,
+                    UserName = revisionDto.User.UserName,
+                    Email = revisionDto.User.Email
+                } : null
+            };
+        }
+
+        public virtual Attributes? ConvertToAttributes(AttributeDto? attributeDto)
+        {
+            if (attributeDto == null) return null;
+            return new Attributes
+            {
+                Id = attributeDto.Id,
+                Color = attributeDto.Color,
+                Material = attributeDto.Material,
+                Model = attributeDto.Model
+            };
+        }
+
+        public virtual Availability? ConvertToAvailability(AvailabilityDto? availabilityDto)
+        {
+            if (availabilityDto == null) return null;
+
+            return new Availability
+            {
+                Id = availabilityDto.Id,
+                IsAvailable = availabilityDto.IsAvailable,
+                EstimatedDeliveryDate = availabilityDto.EstimatedDeliveryDate
+            };
+        }
+
+        public virtual Image? ConvertToImage(ImageDto? imageDto)
+        {
+            if (imageDto == null) return null;
+
+            return new Image
+            {
+                Id = imageDto.Id,
+                Url = imageDto.Url,
+                AltText = imageDto.AltText
+            };
+        }
+    }
+}
